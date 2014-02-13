190812e94e73bb7d1e71271e59eb0b536a0f1c52
nickpatel82 <to.nickpatel@gmail.com>
2014-02-12 21:52:24 -0700

    First Commit! Learning

---
+++ helloWorld/src/org/prac/helloworld/MyFirstTest.java
@@ -0,0 +1,17 @@
+package org.prac.helloworld;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyFirstTest {
+
+	@Test
+	public void test() {
+		WebDriver driver = new FirefoxDriver();
+		
+	}
+
+}
